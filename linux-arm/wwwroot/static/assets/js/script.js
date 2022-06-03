24716774bbb5c48a2d7957d00af0e7a57d617457
sanglesoft <35786475+sanglesoft@users.noreply.github.com>
2022-06-03 16:07:01 +0700

    Add files via upload

---
+++ linux-arm/wwwroot/static/assets/js/script.js
@@ -0,0 +1,40 @@
+﻿
+$(document).ready(function () {
+    
+    $(".dataTables_length select").addClass("form-control");
+    $(".dataTables_filter input").addClass("form-control");
+});
+
+if (0 < $(".date-picker").length) {
+    $(".date-picker").pickadate({
+        format: "dd/mm/yyyy",
+        showMonthsShort: true,
+        showWeekdaysFull: false,
+        editable: false,
+        selectYears: true,
+        selectMonths: true,
+        closeOnSelect: true,
+        closeOnClear: true,
+    });
+}
+
+var isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
